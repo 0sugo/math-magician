6b158651438d616ade3659ed9b2fbdf37a041a50
0sugo <josugo26@gmail.com>
2023-04-26 19:46:48 +0300

    Fix requested changes

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+export default function Button() {
+  return (
+    <div>
+      <ul className="digits">
+        <li>AC</li>
+        <li>+/-</li>
+        <li>%</li>
+        <li className="operations">/</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li className="operations">*</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li className="operations">-</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li className="operations">+</li>
+        <li className="zero">0</li>
+        <li>.</li>
+        <li className="operations">=</li>
+      </ul>
+    </div>
+  );
+}
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,11 @@
+import Button from './Button';
+
 export default function Calculator() {
   return (
     <main className="combined">
       <div className="display">0</div>
-      <div>
-        <ul className="digits">
-          <li>AC</li>
-          <li>+/-</li>
-          <li>%</li>
-          <li className="operations">/</li>
-          <li>7</li>
-          <li>8</li>
-          <li>9</li>
-          <li className="operations">*</li>
-          <li>4</li>
-          <li>5</li>
-          <li>6</li>
-          <li className="operations">-</li>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li className="operations">+</li>
-          <li className="zero">0</li>
-          <li>.</li>
-          <li className="operations">=</li>
-        </ul>
-      </div>
+      <Button />
+
     </main>
   );
 }

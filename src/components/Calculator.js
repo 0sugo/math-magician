4c0c0140d861f17516b6fd3952571942ff01ce96
0sugo <josugo26@gmail.com>
2023-04-28 00:10:14 +0300

    Add props and states

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,18 @@
+import { useState } from 'react';
 import Button from './Button';
 import Calculate from '../logic/Calculate';
-import Operate from '../logic/Operate';
 
 export default function Calculator() {
+  const [show, setShow] = useState({ total: null, next: null, operation: null });
   return (
     <main className="combined">
-      <div className="display">0</div>
-      <Button />
-
+      <div className="display">{show.next || show.total || 0 }</div>
+      <Button fetcher={(e) => {
+        const valueClicked = e.target.innerHTML;
+        const result = Calculate(show, valueClicked);
+        setShow(result);
+      }}
+      />
     </main>
   );
 }

d89989783354ee7943fdf1c58c55b62801c31357
0sugo <josugo26@gmail.com>
2023-04-28 19:36:42 +0300

    Add useEffect to fetch from API

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from './Button';
 import Calculate from '../logic/Calculate';
+import Ninja from './Ninja';
 
 export default function Calculator() {
   const [show, setShow] = useState({ total: null, next: null, operation: null });
+  const [quote, setQuote] = useState([]);
+  useEffect(() => {
+    fetch('https://api.api-ninjas.com/v1/quotes?category=education', {
+      method: 'GET',
+      headers: { 'X-Api-key': 'CxQhbgnKF2X1iPThUiuulA==2qSxWFNNTyvLkSvt' },
+    })
+      .then((resp) => resp.json())
+      .then((data) => setQuote(data[0].quote));
+  }, []);
+
   return (
     <main className="combined">
       <div className="display">{show.next || show.total || 0 }</div>
@@ -13,6 +24,7 @@ export default function Calculator() {
         setShow(result);
       }}
       />
+      <Ninja RandomNinja={`"${quote}"`} />
     </main>
   );
 }

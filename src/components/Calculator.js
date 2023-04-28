6b557e4bf9850a40af77bda6b312c7eae3bb76ce
0sugo <josugo26@gmail.com>
2023-04-28 20:37:12 +0300

    Remove useEffect and rendering jsx

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,19 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Button from './Button';
 import Calculate from '../logic/Calculate';
-import Ninja from './Ninja';
 
 export default function Calculator() {
   const [show, setShow] = useState({ total: null, next: null, operation: null });
-  const [quote, setQuote] = useState([]);
-  useEffect(() => {
-    fetch('https://api.api-ninjas.com/v1/quotes?category=education', {
-      method: 'GET',
-      headers: { 'X-Api-key': 'CxQhbgnKF2X1iPThUiuulA==2qSxWFNNTyvLkSvt' },
-    })
-      .then((resp) => resp.json())
-      .then((data) => setQuote(data[0].quote));
-  }, []);
 
   return (
     <main className="combined">
@@ -24,7 +14,6 @@ export default function Calculator() {
         setShow(result);
       }}
       />
-      <Ninja RandomNinja={`"${quote}"`} />
     </main>
   );
 }
